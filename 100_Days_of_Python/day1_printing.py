b68c4d22f5b4a7616437b84c9cf99c5a572915ea
BrizaEvantai <121634004+BrizaEvantai@users.noreply.github.com>
2023-01-02 10:04:27 +0100

    Add files via upload

---
+++ 100_Days_of_Python/day1_printing.py
@@ -0,0 +1,11 @@
+# This is how you use the print function in Python. You can use both single
+# and double quotation marks. For now, you can simply use single when your
+# text contains double, and double when your text contains single.
+
+
+""" You can add comments using the # sign, or longer comments by writing the
+text in between 3 double quotation marks at the beginning of the text and
+3 double quotation marks at the end of the text. """
+
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
